36738f7e7616137e2668c5b37841d4929d25b4c6
VasyaRudyi <153776699+VasyaRudyi@users.noreply.github.com>
2024-01-21 17:23:36 +0000

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+# take in the user's name using input() function
+# print it using print() function
+# do the same with user's age
+# print out Hello World using print() funciton
+
+
+user_name = input("Enter your name: ")
+print (user_name)
+user_age = input("Enter your age: ")
+print (user_age)
+print ("Hello World!")
