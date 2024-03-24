17a463906850f65eb6d258493adc9dc6e730a8b4
Vasyl Rudyi <Vasyl01@protonmail.com>
2024-03-24 19:10:12 +0000

    r_e is changed, docstring is added

--- while.py
+++ while.py
@@ -4,13 +4,14 @@
 # for this create function to check the string from user (could not find any other function)
 
 def isfloat(str_ing):
+    'The function accepts string parameter, and returns True if its rational numbers or False if not.'
     ch_eck = "0123456789."
     if str_ing[0] == '-':
         str_ing = str_ing[1::] # rewrite the string to allow negative input
-    r_e = True # r_e keep what is have to be return
+    bool_r = True # bool_r keep what is have to be return
     if str_ing[0] == '.': # compare first char if it start with '.' If yes its not float
-        r_e = False
-        return r_e
+        bool_r = False
+        return bool_r
     c_ount = 0 # count the amount of '.' if more than 2 - the string cannot be float
     for i in str_ing: # iterate over incoming string
         if i == '.':
@@ -18,12 +19,13 @@ def isfloat(str_ing):
         for y in ch_eck: # iterate over sample chars
             if i == y:
                 break
-            if y == '.': # if counter 'y' comes to the end keep '.' - no matches found. So some other char is present in the string.
-                r_e = False
+            if y == '.': # if counter 'y' comes to the end keep '.' - no matches found. 
+                         # So some other char is present in the string.
+                bool_r = False
         if c_ount > 1:
-            r_e = False
+            bool_r = False
             break
-    return r_e
+    return bool_r
 
 num = 0
 total = 0
